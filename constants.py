06eb673563f3cd7f5c0ae6d677b66544cc6ff72e
hellbuster45 <rupenkumar22@gmail.com>
2023-10-29 15:40:08 +0530

    files uploaded..

---
+++ constants.py
@@ -0,0 +1,22 @@
+import pygame as po
+
+# game variables
+FPS = 80
+GRAVITY = 0.3
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900 
+TILE_SIZE = 40
+
+# player action variables
+CHAR_IDLE = 0
+CHAR_JUMP = 1
+CHAR_DEATH = 2
+CHAR_RUN = 3
+CHAR_CROUCH = 4
+CHAR_CLIMB = 5
+CHAR_HURT = 6
+
+# groups
+enemy_group = po.sprite.Group()
+item_group = po.sprite.Group()
+bullet_group = po.sprite.Group()
