9957b81400952abc7bc14c4e8f14668f9eb89c03
hellbuster45 <rupenkumar22@gmail.com>
2023-11-14 00:55:18 +0530

    game finished with bugs..

--- Tilesets/Industrial_Zone/Objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="32" tileheight="32" tilecount="42" columns="7">
- <image source="../../assets/actual_assets/craftpix-net-965049-free-industrial-zone-tileset-pixel-art/PSD/Objects.png" width="224" height="192"/>
-</tileset>
